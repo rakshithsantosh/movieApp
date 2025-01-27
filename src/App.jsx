041be487375e2c5cb73de4c7eba8edfb47b090d4
rakshith_s <rakshithsantosh@gmail.com>
2025-01-27 12:22:06 +0530

    project setup with tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+const App = () => {
+  return (
+    <main>
+      <div className="pattern"></div>
+      <div className="wrapper">
+        <header>
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy{" "}
+          </h1>
+        </header>
+        <p>Search</p>
+      </div>
+    </main>
+  );
+};
+
+export default App;
